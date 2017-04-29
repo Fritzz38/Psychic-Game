b1316360a778063b8cd67f0d3aa831bd0de284fa
Tarmin Sidharta <Tarmin Sidharta>
2017-04-29 10:01:16 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,9 @@
         var wins = 0;
         var losses = 0;
         var chances = 9;
-        var userGuess="";
-                
+        var userGuess = "";
+        
+            
         var gen = document.getElementById("m");
         gen.innerHTML = wins;
 
@@ -14,65 +15,55 @@
         var don = document.getElementById("nom");
         don.innerHTML = chances;
 
+        var g = document.getElementById("letter");
 
         var options = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
 
         var computerGuess = options [Math.floor(Math.random() * options.length)];
+         
+        console.log(computerGuess);
 
-        console.log(computerGuess);   
+        
     
         document.onkeyup = function (event) {
-	    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        dumbledore(userGuess, computerGuess);
-        arr+= userGuess + ", ";
-        var g = document.getElementById("letter");
-        g.innerHTML = arr;
+	            userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+                arr+= userGuess + ", ";
+                g.innerHTML = arr;
+                dumbledore(userGuess, computerGuess);
 	  	}
         
-                
-        function dumbledore (userGuess,computerGuess) {
-            if (computerGuess == userGuess) {
+        
+         function reset() {
+            arr = "";
+            g.innerHTML = arr;
+            chances = 9;
+            don.innerHTML = chances;
+            computerGuess = options [Math.floor(Math.random() * options.length)];
+         }
+            
+
+         function dumbledore (userGuess,computerGuess) {
+            
+            if (userGuess == computerGuess) {
                  wins++;
-                 var gen = document.getElementById("m");
                  gen.innerHTML = wins;
-                 computerGuess = options [Math.floor(Math.random() * options.length)];
-
+                 reset();
+                         
             }
 
-            else {
-                chances--;
-                var don = document.getElementById("nom");
-                don.innerHTML = chances;
+            else if (userGuess != computerGuess) {
+                  chances--;
+                  don.innerHTML = chances;
 
-                if (chances === 0) {
-                    losses++;  
-                    var fud = document.getElementById("k");
-                    fud.innerHTML = losses;               
-                    chances = 9;
-                    computerGuess = options [Math.floor(Math.random() * options.length)];}
+                  if (chances === 0) {
+                             losses++;  
+                             fud.innerHTML = losses;  
+                             reset();
+                    }
 
             }
-
+            
+            
         }
-
-        // computer need to produce random character after:
-
-        // 1. user wins OR
-
-        // 2. user losses (used up all chances)
-
-     
-        
-          
-      
-        /* user_guessed = [];
-
-         user_guessed.push(" " + userGuess);
-        document.getElementById("userguessed").innerHTML = user_guessed;*/
-        
-
-
  
-
-
 
